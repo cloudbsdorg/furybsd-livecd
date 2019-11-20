b4445006b3bedbdf4f323da201cff815ac16e247
pkgdemon <pkgdemonteam@gmail.com>
2019-11-20 04:23:13 -0500

    Disable init chroot

--- overlays/ramdisk/init.sh
+++ overlays/ramdisk/init.sh
@@ -21,57 +21,57 @@ done
 
 echo "==> Mount cdrom"
 mount_cd9660 /dev/iso9660/FURYBSD /cdrom
-mdmfs -P -F /cdrom/data/system.uzip -o ro md.uzip /sysroot
+#mdmfs -P -F /cdrom/data/system.uzip -o ro md.uzip /sysroot
 
-if [ "$SINGLE_USER" = "true" ]; then
-	echo -n "Enter memdisk size used for read-write access in the live system: "
-	read MEMDISK_SIZE
-else
-	MEMDISK_SIZE="1024"
-fi
+#if [ "$SINGLE_USER" = "true" ]; then
+#	echo -n "Enter memdisk size used for read-write access in the live system: "
+#	read MEMDISK_SIZE
+#else
+#	MEMDISK_SIZE="1024"
+#fi
 
-echo "==> Mount swap-based memdisk"
-mdmfs -s "${MEMDISK_SIZE}m" md /memdisk || exit 1
-mount -t unionfs /memdisk /sysroot
+#echo "==> Mount swap-based memdisk"
+#mdmfs -s "${MEMDISK_SIZE}m" md /memdisk || exit 1
+#mount -t unionfs /memdisk /sysroot
 
-mkdir -p /sysroot/media/cdrom
-mount_nullfs -o ro /cdrom /sysroot/media/cdrom
+#mkdir -p /sysroot/media/cdrom
+#mount_nullfs -o ro /cdrom /sysroot/media/cdrom
 
-mount -t tmpfs tmpfs /sysroot/tmp
-mount -t tmpfs tmpfs /sysroot/mnt
-mount -t nullfs /sysroot/tmp /tmp
-mount -t nullfs /sysroot/mnt /mnt
+#mount -t tmpfs tmpfs /sysroot/tmp
+#mount -t tmpfs tmpfs /sysroot/mnt
+#mount -t nullfs /sysroot/tmp /tmp
+#mount -t nullfs /sysroot/mnt /mnt
 
-echo "==> Mount devfs"
-mount -t devfs devfs /sysroot/dev
+#echo "==> Mount devfs"
+#mount -t devfs devfs /sysroot/dev
 
-BOOTMODE=`sysctl -n machdep.bootmethod`
-export BOOTMODE
+#BOOTMODE=`sysctl -n machdep.bootmethod`
+#export BOOTMODE
 
-if [ "${BOOTMODE}" = "BIOS" ]; then
-  echo "BIOS detected"
-  cp /sysroot/usr/home/liveuser/xorg.conf.d/driver-vesa.conf /sysroot/etc/X11/xorg.conf
-fi
+#if [ "${BOOTMODE}" = "BIOS" ]; then
+#  echo "BIOS detected"
+#  cp /sysroot/usr/home/liveuser/xorg.conf.d/driver-vesa.conf /sysroot/etc/X11/xorg.conf
+#fi
 
-if [ "${BOOTMODE}" = "UEFI" ]; then
-  echo "UEFI detected"
-  cp /sysroot/usr/home/liveuser/xorg.conf.d/driver-scfb.conf /sysroot/etc/X11/xorg.conf
-fi
+#if [ "${BOOTMODE}" = "UEFI" ]; then
+#  echo "UEFI detected"
+#  cp /sysroot/usr/home/liveuser/xorg.conf.d/driver-scfb.conf /sysroot/etc/X11/xorg.conf
+#fi
 
-VMGUEST=`sysctl -n kern.vm_guest`
-export VMGUEST
+#VMGUEST=`sysctl -n kern.vm_guest`
+#export VMGUEST
 
-if [ "${VMGUEST}" = "xen" ]; then
-  echo "XEN guest detected"
-  chroot /sysroot sysrc devd_enable="NO"
-fi
+#if [ "${VMGUEST}" = "xen" ]; then
+#  echo "XEN guest detected"
+#  chroot /sysroot sysrc devd_enable="NO"
+#fi
 
-chroot sysroot sysrc -f /etc/rc.conf kld_list+="sysctlinfo"
+#chroot sysroot sysrc -f /etc/rc.conf kld_list+="sysctlinfo"
 
-if [ "$SINGLE_USER" = "true" ]; then
+#if [ "$SINGLE_USER" = "true" ]; then
 	echo "Starting interactive shell in temporary rootfs ..."
 	sh
-fi
+#fi
 
-kenv init_shell="/bin/sh"
+kenv init_shell="/rescue/sh"
 exit 0
