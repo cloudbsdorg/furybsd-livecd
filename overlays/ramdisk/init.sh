ec808738a0482d3ddfe6f637732ce085d6a5de59
pkgdemon <pkgdemonteam@gmail.com>
2019-11-20 06:26:24 -0500

    Increase size of memdisk

--- overlays/ramdisk/init.sh
+++ overlays/ramdisk/init.sh
@@ -27,7 +27,7 @@ if [ "$SINGLE_USER" = "true" ]; then
 	echo -n "Enter memdisk size used for read-write access in the live system: "
 	read MEMDISK_SIZE
 else
-	MEMDISK_SIZE="2048"
+	MEMDISK_SIZE="6144"
 fi
 
 echo "==> Mount swap-based memdisk"
